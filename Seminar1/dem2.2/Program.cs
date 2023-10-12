93299cdcd5fd9de71e783fe7dcbf3d1260caea7f
Amletick <VadimV2001@gmail.com>
2023-10-12 21:09:15 +0300

    pr1

---
+++ Seminar1/dem2.2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1");
+string num1str=Console.ReadLine();
+int num1=Convert.ToInt32(num1str);
+Console.WriteLine("Введите число 2");
+string num2str=Console.ReadLine();
+int num2=Convert.ToInt32(num2str);
+if (num1==num2*num2)
+{
+    Console.Write("a = "+num1+", b = "+num2+"-> Да");
+}
+else
+{
+    Console.Write("a = "+num1+", b = "+num2+"-> нет");
+}
---
+++ Seminar1/dem4/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] array = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота","Воскресенье"};
+ Console.WriteLine("Введите номер дня");   
+string daystring = Console.ReadLine();
+int daynumber = Convert.ToInt32(daystring);
+if ((daynumber>=1)&&(daynumber<8))
+{
+Console.WriteLine(daynumber +" -> "+ array[daynumber-1]);
+}
+else
+{
+ Console.WriteLine("Нет такого номера дня");   
+}
