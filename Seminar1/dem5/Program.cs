c14b5e10a58a8a2c28d181b15a0c6c9c7adb3a0d
Amletick <VadimV2001@gmail.com>
2023-10-12 22:34:41 +0300

    num57

--- Seminar1/dem5/Program.cs
+++ Seminar1/dem5/Program.cs
@@ -1,3 +1,16 @@
 ﻿string numstr = Console.ReadLine();
-int num = Convert.ToInt32(daystring);
+int num = Convert.ToInt32(numstr);
 //условие на отрицательность
+if (num<0) 
+{
+    Console.WriteLine();
+}
+else
+{
+    int i=-num;
+    while (i<=num) 
+    {
+        Console.Write(i+" ");
+        i++;
+    }
+}
---
+++ Seminar1/dem6/Program.cs
@@ -0,0 +1,9 @@
+﻿string numstr = Console.ReadLine();
+if (numstr.Length!=3) 
+{
+    Console.WriteLine("Число не трехзначное");
+}
+else
+{
+       Console.Write(numstr[2]);//нумерация с 0 3-1=2
+}
