091217e6e8261d5a9075a2edcc737626ba5b6f14
Amletick <VadimV2001@gmail.com>
2023-10-12 20:43:07 +0300

    sqrcheck

---
+++ Seminar1/dem2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число 1");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2=Convert.ToInt32(Console.ReadLine());
+int sqr1=num1*num1;
+int sqr2=num2*num2;
+if  ((num1==sqr2)|(num2==sqr1))
+{
+    Console.WriteLine("Одно число квадрат другого");    
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратом друг друга");    
+}
