6849ef67a74930408d67d5d00832158f4da61089
Amletick <VadimV2001@gmail.com>
2023-10-13 00:38:09 +0300

    dzdone

---
+++ Homework/dz1test/Program.cs
@@ -0,0 +1,9 @@
+﻿ int number=Convert.ToInt32(Console.ReadLine());
+
+  int i=2;
+      Console.Write("number = "+number+" -> ");
+        while (i<= number)
+        {
+          Console.Write(i+"  ");
+          i+=2;
+        }
