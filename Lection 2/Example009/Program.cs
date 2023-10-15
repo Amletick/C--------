7ec9760a140f9c9e25227a11d9f2ea7bde5d84d7
Amletick <VadimV2001@gmail.com>
2023-10-15 19:53:26 +0300

    Find

---
+++ Lection 2/Example009/Program.cs
@@ -0,0 +1,18 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result= arg1;
+    if (arg2>result)
+    {
+        result=arg2;
+    }
+        if (arg3>result)
+    {
+        result=arg3;
+    }
+    return result;
+}
+int[] array={10,2456,1321,6485,1345,13248,3248,4652,1324,};
+int max=Max(Max(array[0],array[1],array[2]),
+            Max(array[3],array[4],array[5]),
+            Max(array[6],array[7],array[8]));
+Console.WriteLine(max);
---
+++ Lection 2/example010/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array={781,672,356,434,456,636,34,736,456,664,465,534,23,657,89,342,546};
+int n = array.Length;
+int find = 342;
+int i=0;
+int flag=0;
+while (i<n-1)
+{
+    if (array[i]==find)
+    {
+        Console.WriteLine("Нашёл");
+        flag=1;
+        break;
+    }
+    i++;
+}
+
+if (flag==0)
+{
+    Console.WriteLine("Не нашёл");
+}
