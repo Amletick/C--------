d5140821ce5aae6a9f2889e4a3fc4624f961f77f
Amletick <VadimV2001@gmail.com>
2023-10-23 23:42:54 +0300

    HW4

--- Homework/HW4/num1/Program.cs
+++ Homework/HW4/num1/Program.cs
@@ -1,9 +1,28 @@
 ﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 3, 5 -> 243 (3⁵)
-2, 4 -> 16
+2, 4 -> 16*/
 
-Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-452 -> 11
-82 -> 10
-9012 -> 12
+int Input(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double pow_for_poor(int a, int b)
+{
+    double temppow=Convert.ToDouble(a);
+    for (int i=2;i<=b;i++)
+    {
+        temppow=temppow*a;
+    }
+    return temppow;
+}
 
+void Output(double result,string message)
+{
+    Console.Write(message);
+    Console.WriteLine(result);
+}
+int a=Input("Введите число А");
+int b=Input("Введите число B");
+double result=pow_for_poor(a,b);
+Output(result,"Результат: ");
--- Homework/HW4/num2/Program.cs
+++ Homework/HW4/num2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int Input(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+double LengthCountMath(int a)
+{
+    int sum=0;
+    while(a!=0)
+    {
+            sum=sum+a%10;
+            a=a/10;            
+    }
+    return sum;
+}
+
+void Output(double result,string message)
+{
+    Console.Write(message);
+    Console.WriteLine(result);
+}
+int a=Input("Введите число А");
+double result=LengthCountMath(a);
+Output(result,"Результат: ");
