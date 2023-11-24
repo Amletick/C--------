9a5448f489bd616fa04de5ae7e601bb1d518863c
Amletick <VadimV2001@gmail.com>
2023-11-24 18:39:57 +0300

    num64FYN

--- FYN(HW9)/num 64/Program.cs
+++ FYN(HW9)/num 64/Program.cs
@@ -2,5 +2,32 @@
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
+      static void PrintNaturalsReverse(int n)
+    {
+        if (n > 1)
+        {
+            Console.Write($"{n}, ");
+            PrintNaturalsReverse(n - 1);
+        }
+        else if (n == 1)
+        {
+            Console.Write(n);
+        }
+    }
 
-
+Console.WriteLine("Введите число N:");
+if (int.TryParse(Console.ReadLine(), out int N))
+        {
+            if (N >= 1)
+            {
+                PrintNaturalsReverse(N);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели число меньше 1, натуральных чисел меньше 1 нет.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число.");
+        }
